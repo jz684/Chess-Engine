ebee107ae6782176733a222a3f0002aa3014fd45
Jackson Lee <jacksonjealee@gmail.com>
2025-10-21 09:35:49 -0400

    Wrote the chessFrame class

---
+++ src/ChessFrame.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ChessFrame extends JFrame {
+
+    private ChessGame chessGame;
+
+    public ChessFrame(ChessPanel chessPanel, ChessGame chessGame) {
+        this.chessGame = chessGame;
+        this.add(chessPanel);
+        this.setTitle("Chess");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setIconImage(createLogo());
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+
+    // TODO
+//    public MenuBar createMenu() {}
+
+    public Image createLogo() {
+        String path = "images/chessImage.png";
+        ImageIcon icon = new ImageIcon(path);
+        return icon.getImage();
+    }
+}
